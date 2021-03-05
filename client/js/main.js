b12ee38008500aca5576e650f6fbe3919352fc4e
J-R4 <JoshuaFanera@gmail.com>
2021-03-05 09:31:02 +0700

    want to merge

--- client/js/main.js
+++ client/js/main.js
@@ -1,85 +1,80 @@
-let baseURL = ''
+let baseURL = '';
 
-$(document).ready(function() {
-  e.preventDefault();
-
-  view()
-
-  $("#login-form").on("submit", (e) => {
+$(document).ready(function () {
     e.preventDefault();
 
-    login()
-  })
-  $("#register-form").on("submit", (e) => {
-    e.preventDefault();
+    view();
 
-    register()
-  })
-  $('#logout-btn').on('click', (e) => {
-    e.preventDefault()
+    $('#login-form').on('submit', (e) => {
+        e.preventDefault();
 
-    logout()
-  })
-})
+        login();
+    });
+    $('#register-form').on('submit', (e) => {
+        e.preventDefault();
+
+        register();
+    });
+    $('#logout-btn').on('click', (e) => {
+        e.preventDefault();
+
+        logout();
+    });
+});
 
 function view() {
-  if(!localStorage.getItem('access_token')) {
-    $('#login-page').show()
-    $('#navbar').hide()
-    $('#home-page').hide()
-    $('#all-destination').hide()
-  } else {
-    $('#login-page').hide()
-    $('#navbar').show()
-    $('#home-page').show()
-    $('#all-destination').show()
-  }
+    if (!localStorage.getItem('access_token')) {
+        $('#login-page').show();
+        $('#navbar').hide();
+        $('#home-page').hide();
+        $('#all-destination').hide();
+    } else {
+        $('#login-page').hide();
+        $('#navbar').show();
+        $('#home-page').show();
+        $('#all-destination').show();
+    }
 }
 
-
 function login() {
-  let email = $("#login-email").val()
-  let password = $("#login-password").val()
-  $.ajax({
-    url: baseURL + "login",
-    method: "POST",
-    data: {
-      email,
-      password
-    }
-  })
-  .done(({ access_token }) => {
-    localStorage.setItem("access_token", access_token)
-    view()
-  })
-  fail((xhr, test) =>{
-    console.log({xhr, test})
-  })
-  .always(() => $("#login-form").trigger("reset"))
+    let email = $('#login-email').val();
+    let password = $('#login-password').val();
+    $.ajax({
+        url: baseURL + 'login',
+        method: 'POST',
+        data: {
+            email,
+            password,
+        },
+    }).done(({ access_token }) => {
+        localStorage.setItem('access_token', access_token);
+        view();
+    });
+    fail((xhr, test) => {
+        console.log({ xhr, test });
+    }).always(() => $('#login-form').trigger('reset'));
 }
 
 function register() {
-  let email = $("#regis-email").val()
-  let password = $("#regis-password").val()
-  $.ajax({
-    url: baseURL + "register",
-    method: "POST",
-    data: {
-      email,
-      password
-    }
-  })
-  .done(({ access_token }) => {
-    localStorage.setItem("access_token", access_token)
-    view()
-  })
-  fail((xhr, test) =>{
-    console.log({xhr, test})
-  })
-  .always(() => $("#register-form").trigger("reset"))
+    let email = $('#regis-email').val();
+    let password = $('#regis-password').val();
+    $.ajax({
+        url: baseURL + 'register',
+        method: 'POST',
+        data: {
+            email,
+            password,
+        },
+    }).done(({ access_token }) => {
+        localStorage.setItem('access_token', access_token);
+        view();
+    });
+    fail((xhr, test) => {
+        console.log({ xhr, test });
+    }).always(() => $('#register-form').trigger('reset'));
 }
 
 function logout() {
-  localStorage.removeItem('access_token')
-  view()
+    localStorage.removeItem('access_token');
+    view();
 }
